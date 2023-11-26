5a6df7694801969666eca7f2c3f17b175eaab5b2
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-11-26 18:25:17 -0500

    Write the tables model

--- src/config/db.js
+++ src/config/db.js
@@ -5,8 +5,8 @@ dotenv.config()
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-	host: process.env.HOST,
-	user: process.env.USER,
+	host: 'localhost',
+	user: 'root',
 	password: process.env.PASSWORD,
 	database: process.env.DB,
 });
