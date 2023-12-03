20162657ff861b9ef44ea633ebe6748f007ba568
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-12-03 16:17:17 -0500

    signup functionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import mysql from "mysql";
+import dotenv from "dotenv";
+import app from "./app.js";
+dotenv.config();
 
-const db = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'CHUrabrady123@',
-  });
-
-  db.connect((err) => {
-	if (err) {
-	  console.error('MySQL connection error:', err);
-	} else {
-	  console.log('Connected to MySQL database');
-	}})
+try {
+	const port = process.env.PORT || 3000;
+	app.listen(port, () => {
+		console.log(
+			`🚀 Daily Tracker service is ready at http://localhost:${port}`
+		);
+	});
+} catch (error) {
+	console.log("Error ==>", error);
+	process.exit(1);
+}
