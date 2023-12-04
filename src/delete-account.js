99b141a98c25f62840eedde2ed53c5bcc66053ef
olaniyanpelumi <olaniyanp11@gmail.com>
2023-12-04 04:28:42 +0100

    created feature/delete-account branch , created /delete/:id api all in delete-account file

--- src/delete-account.js
+++ src/delete-account.js
@@ -1,9 +1,18 @@
-import app from "./app";
+import app from "./app.js";
 import { successResponse } from "./utils/libs/response";
+import connection from "./config/db.js";
 
-app.get("/del", (req, res) => {
-  return successResponse(
-    res,
-    "the del route🚀"
-  );
+app.get("/delete/:id", (req, res) => {
+  let userid = req.params.id;
+  console.log(req.method);
+  let sql = `DELETE FROM users WHERE userid=${userid}`;
+  connection.query(sql, (error, user) => {
+    console.log(req.method);
+    if (error) {
+      console.log("Error deleting user account :", error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    } else console.log(`user account has been deleted`);
+  });
+  connection.end;
+  return successResponse(res, "user account deleted successfully🚀");
 });
