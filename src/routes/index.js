6b3fe6fe2c7ac83e6a3da8795bf1918b82985f7c
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-12-10 16:45:23 -0500

    Revamp delete user endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import registerRoute from "./register.route.js";
+import deleteRoute from "./delete.route.js";
 
 const router = Router();
 
 registerRoute(router);
+deleteRoute(router);
 
 export default router;
