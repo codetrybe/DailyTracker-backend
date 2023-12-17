9653fe2b9f2f56de3d587c06f0e3c12acecb8bda
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-12-17 15:58:41 -0500

    Revamped email verification endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,13 @@ import { Router } from "express";
 import registerRoute from "./register.route.js";
 import deleteRoute from "./delete.route.js";
 import loginRoute from "./login.route.js";
+import verifyRoute from "./verify.route.js";
 
-const router = express.Router();
+const router = Router();
 
-// POST /verify-email
-router.post('/verify-email', verifyEmail);
 registerRoute(router);
 loginRoute(router);
 deleteRoute(router);
+verifyRoute(router);
 
 export default router;
