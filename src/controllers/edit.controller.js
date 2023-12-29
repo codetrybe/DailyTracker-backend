9dddb93f6818251701959031611c7df770878dde
Abubakar Adam Sani <Athernee@gmail.com>
2023-12-29 18:04:13 +0100

    Updated query, paswword hashing and comparison

--- src/controllers/edit.controller.js
+++ src/controllers/edit.controller.js
@@ -5,7 +5,6 @@ import tryCatch from "../utils/libs/tryCatch.js";
 import db from "../config/db.js";
 import util from "util";
 
-
 // convert the callback-based db.query to a promise-based function
 const queryPromise = util.promisify(db.query).bind(db);
 
@@ -28,26 +27,29 @@ export const update = tryCatch(async (req, res) => {
 	}
 
 	// check if user is in db
+	// Unwrap user object from query-returned array, for easy access of user properties.
 	const checkUserQuery = "SELECT * FROM users WHERE user_id = ?";
-	const user = await queryPromise(checkUserQuery, [userId]);
+	const [user] = await queryPromise(checkUserQuery, [userId]);
 
 	if (user.length === 0) {
 		return errorResponse(res, "User not found", StatusCodes.NOT_FOUND);
 	}
 
-	// If user provided password to change
+	// If password is provided in request body, Isolate it.
 	if ('password_hash' in requestBody) {
 		const newPassword = requestBody['password_hash'];
 		delete requestBody['password_hash'];
 
-		// Check if new pass is same with current pass
-		if (comparePassword(newPassword, user.hashPassword)) {
-			return errorResponse(res, "Same password provided. Please use a different password", StatusCodes.NO_CONTENT);
+		// Verify if new pass is same with current pass
+		const { password_hash } = user;
+		if (await comparePassword(newPassword, password_hash)) {
+			return errorResponse(res, "Same password provided. Please use a different password", StatusCodes.BAD_REQUEST);
 		}
 
 		// Hash new pass and update the database with it
-		const newPasswordHashed = hashPassword(newPassword);
-		await queryPromise("UPDATE users SET ? WHERE user_id = ?", [newPasswordHashed, userId]);
+		const newPasswordHashed = await hashPassword(newPassword);
+		const newQuery = "UPDATE users SET password_hash = ? WHERE user_id = ?";
+		await queryPromise(newQuery, [newPasswordHashed, userId]);
 	}
 
 	// Update allowed fields if user is found
