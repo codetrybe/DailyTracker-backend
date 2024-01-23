5b41abf9e73efaeab408b37811a86975385be4b0
kalwhyte <superkalel55@gmail.com>
2024-01-23 09:22:05 +0100

    feat: Implement task management API endpoints
    Added API endpoints for adding, editing, and deleting tasks in the todo list. The implementation includes handling task details, validation, and database interactions. Also, placeholders for TODO comments are included for specific implementation details. Update routes to use these new endpoints.

---
+++ src/controllers/task-management.js
@@ -0,0 +1,67 @@
+import db from '../config/db';
+import utils from '../utils';
+import tryCatch from '../utils/libs/tryCatch.js';
+import { errorResponse, successResponse } from '../utils/libs/response';
+import { statusCodes } from 'http-status-codes';
+
+const queryPromise = util.promisify(db.query).bind(db);
+
+// Create a new task
+export const add_task = tryCatch(async (req, res) => {
+    /**
+     * TODO:
+     * Get the task name from the request body
+     * Validate the task name
+     * Insert the task name into the database
+     */
+    const { task_name } = req.body;
+
+    const insertTaskQuery = `INSERT INTO tasks (task_name) VALUES (?)`;
+    const insertTaskValues = [task_name];
+
+    const insertTaskResult = await queryPromise(insertTaskQuery, insertTaskValues);
+    const insertTaskId = insertTaskResult.insertId;
+
+    return successResponse(res, statusCodes.CREATED, 'Task created successfully', { task_id: insertTaskId });
+});
+
+
+// Edit an existing task
+export const edit_task = tryCatch(async (req, res) => {
+    /**
+     * TODO:
+     * Get the task ID from the request parameters
+     * Get the task name from the request body
+     * Validate the task name
+     * Update the task name in the database
+     * Return a success response
+     */
+    const { task_id } = req.params;
+    const { task_name } = req.body;
+
+    const updateTaskQuery = `UPDATE tasks SET task_name = ? WHERE task_id = ?`;
+    const updateTaskValues = [task_name, task_id];
+
+    await queryPromise(updateTaskQuery, updateTaskValues);
+
+    return successResponse(res, statusCodes.OK, 'Task updated successfully');
+});
+
+
+// Delete an existing task
+export const delete_task = tryCatch(async (req, res) => {
+    /**
+     * TODO:
+     * Get the task ID from the request parameters
+     * Delete the task from the database
+     * Return a success response
+     */
+    const { task_id } = req.params;
+
+    const deleteTaskQuery = `DELETE FROM tasks WHERE task_id = ?`;
+    const deleteTaskValues = [task_id];
+
+    await queryPromise(deleteTaskQuery, deleteTaskValues);
+
+    return successResponse(res, statusCodes.OK, 'Task deleted successfully');
+});
