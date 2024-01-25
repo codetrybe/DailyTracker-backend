4c48fee77a0213f4335194310744fdcf7b272245
Abiodun Shittu <jerrywizklay1@gmail.com>
2024-01-25 03:48:23 -0500

    Add validations to signup and login request body

--- src/routes/userAuth.route.js
+++ src/routes/userAuth.route.js
@@ -1,11 +1,11 @@
 import { forgotpassword, login, register, resendEmailVerification, resetPassword, verifyEmail, verifyPasswordOtp } from "../controllers/userAuth.controller.js";
-import { registerValidator } from "../middlewares/validation/register.validation.js";
+import { loginValidator, registerValidator } from "../middlewares/validation/user.validation.js";
 
 export default (router) => {
   router.post("/users/signUp", registerValidator, register);
   router.post("/users/verifyEmail", verifyEmail);
   router.post("/users/resendEmailVerification", resendEmailVerification);
-  router.post("/users/login", login);
+  router.post("/users/login", loginValidator, login);
   router.post("/users/forgotPassword", forgotpassword);
   router.post("/users/verifyPasswordOtp", verifyPasswordOtp);
   router.post("/users/:user_id/resetPassword", resetPassword);
